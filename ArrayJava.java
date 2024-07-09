7d6b71a49dd34e24a21259a65cc2b86239e422c7
Arpit Raj <113095887+arpitraj96@users.noreply.github.com>
2024-07-09 09:26:06 +0000

    add code for Array

--- ArrayJava.java
+++ ArrayJava.java
@@ -15,6 +15,41 @@ public class ArrayJava {
         }
         System.out.println("The sum of the elements are: "+sum);
         System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(sort(arr)));
+        System.out.println("Odd elements are: ");
+        System.out.println(printOdd(arr));
         sc.close();
+    }
+    static int[] sort(int[] arr){
+        int n = arr.length;
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (swapped == false)
+                break;
+        }
+        return arr;
+    }
+    static int printOdd(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]%2!=0){
+                System.out.print(arr[i]+" ");
+                sum += arr[i];
+            }
+        }
+        System.out.println();
+        return sum;
     }
 }
