1c7b28d9ffb5329c0626b941fc9fb48e38e1caab
Arpit Raj <113095887+arpitraj96@users.noreply.github.com>
2024-07-09 09:25:47 +0000

    add code for Matrix

--- Matrix.java
+++ Matrix.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Matrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rows and columns: ");
+        System.out.print("Enter the number of rows and columns: ");
         int row = sc.nextInt();
         int col = sc.nextInt();
         int[][] matrix = new int[row][col]; 
@@ -14,7 +14,26 @@ public class Matrix {
                 matrix[i][j] = sc.nextInt();
             }
         }
+        for(int i=0; i<row; i++){
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
         System.out.println(Arrays.deepToString(matrix));
+        for(int i=0; i<row; i++){
+            for (int j = 0; j < col; j++) {
+                if(i==j){
+                    System.out.print(matrix[i][j]);
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
         sc.close();
     }
 }
