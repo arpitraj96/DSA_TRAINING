5b14694908bd10d01fbc2e12326c9619cdf2a9de
Arpit Raj <113095887+arpitraj96@users.noreply.github.com>
2024-07-24 04:36:01 +0000

    move problem to the VJudge

--- VJudge/ChefOfTheYear.java
+++ VJudge/ChefOfTheYear.java
@@ -1,7 +1,8 @@
+package VJudge;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
